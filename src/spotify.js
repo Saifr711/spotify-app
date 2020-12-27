703294b31d83962b5d58f7f466ed627dbe60bad7
Waqar <saif@visana.no>
2020-12-27 21:17:03 +0100

    fixed the redirect uri issue. Now the login btn is working correctly and sending the user to the right login form spotify's API.

---
+++ src/spotify.js
@@ -0,0 +1,18 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const redirectUri = "http://localhost:3000/";
+
+const clientId = "fbec335d81f141858fd788fb71edfb4d";
+
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state",
+];
+
+export const loginUrl =
+    `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+        "%20"
+    )}&response_type=token&show_dialog=true`;
