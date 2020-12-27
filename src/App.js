e522fdd063929bb75c4138ecb4e7deb203d5c447
Waqar <saif@visana.no>
2020-12-27 17:30:08 +0100

    restored code from first project folder. Was not able to connect previous folder to Github for multiple reasons. Therefore created this folder to continue the  build of the  Spotify clone.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Login from "./Login";
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="app">
+
+      <Login />
     </div>
   );
 }
 
 export default App;
+
